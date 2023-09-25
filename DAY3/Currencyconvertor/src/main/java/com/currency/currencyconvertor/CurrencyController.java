a5e00a1d2baaa7b6d76e4309837fbf9a61f37cc2
Bryan Castro <91161604+xVrzBx@users.noreply.github.com>
2023-09-25 16:21:47 -0600

    Styles combo-box

--- DAY3/Currencyconvertor/src/main/java/com/currency/currencyconvertor/CurrencyController.java
+++ DAY3/Currencyconvertor/src/main/java/com/currency/currencyconvertor/CurrencyController.java
@@ -1,6 +1,7 @@
 package com.currency.currencyconvertor;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -11,7 +12,17 @@ public class CurrencyController {
     private TextField input1;
     @FXML
     private TextField input2;
+    @FXML
+    private ComboBox<String> comboBox1;
 
+    @FXML
+    private ComboBox<String> comboBox2;
+
+    public void initialize(){
+        comboBox1.getItems().addAll("MXN", "USD", "EUR");
+        comboBox2.getItems().addAll("MXN", "USD", "EUR");
+
+    }
     @FXML
     protected void onConvertButtonClick() {
         input2.setText("Hello word");
