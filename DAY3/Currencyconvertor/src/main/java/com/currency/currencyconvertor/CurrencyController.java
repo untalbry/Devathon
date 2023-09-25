ef4f44de077892b77178bbe8411631ff1568ca0e
Bryan Castro <91161604+xVrzBx@users.noreply.github.com>
2023-09-25 15:59:13 -0600

    Adding styles

--- DAY3/Currencyconvertor/src/main/java/com/currency/currencyconvertor/CurrencyController.java
+++ DAY3/Currencyconvertor/src/main/java/com/currency/currencyconvertor/CurrencyController.java
@@ -2,13 +2,18 @@ package com.currency.currencyconvertor;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class CurrencyController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField input1;
+    @FXML
+    private TextField input2;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onConvertButtonClick() {
+        input2.setText("Hello word");
     }
 }
