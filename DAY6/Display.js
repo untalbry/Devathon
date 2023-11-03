61a60cf9950fe82f7c9b2e15ff5b836371e64b75
xVrzBx <bryancsinformatica@gmail.com>
2023-11-03 11:15:50 -0600

    adding AC and DEL functionality

--- DAY6/Display.js
+++ DAY6/Display.js
@@ -3,13 +3,27 @@ class Display{
         this.displayValorAnterior = displayValorAnterior;
         this.displayValorActual = displayValorActual;
         this.calculator = new Calculadora();
+        this.operatorTipe = undefined;
         this.valorActual = '';
         this.valorAnterior = '';
+    }
+    delete(){
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.printValue();
+    }
+    deleteAll(){
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.operatorTipe = undefined;
+        this.printValue();
     }
     setNumber(number){
         if(number === '.' && this.valorActual.includes('.')) return;
         this.valorActual = this.valorActual + number.toString();
         this.printValue();
+    }
+    setOperator(operator){
+
     }
     printValue(){
         this.displayValorActual.textContent = this.valorActual;
--- DAY6/index.js
+++ DAY6/index.js
@@ -1,9 +1,13 @@
 const displayValorAnterior = document.getElementById('valor-anterior');
 const displayValorActual = document.getElementById('valor-actual');
 const numberButton = document.querySelectorAll('.number');
-const botonesOperadores = document.querySelectorAll('.operator');
+const operatorButton = document.querySelectorAll('.operator');
 
 const display = new Display(displayValorAnterior, displayValorActual);
+
 numberButton.forEach(boton => {
     boton.addEventListener('click', () => display.setNumber(boton.innerHTML));
+});
+operatorButton.forEach(boton => {
+    boton.addEventListener('click', () => display.setOperator(boton.value));
 });
